fa186c303e56689d154d92f47eb9baeff9682e95
vickycao1221 <vickycao1221@vickycao1221s-MacBook-Pro.local>
2015-10-13 22:43:14 +0800

    add file

---
+++ Health/Health/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Health
+//
+//  Created by vickycao1221 on 10/13/15.
+//  Copyright © 2015 vickycao1221. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
